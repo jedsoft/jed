8bc4497a6bc953d1d5374e858799ee42f56017c7
John E. Davis <jed@jedsoft.org>
2013-09-25 22:42:52 +0000

    pre0.99-20 Change 73: swap order of tcsetpgid and setpgid calls.
    On linux, the order does not matter but OpenBSD requires setpgid to come first.

--- src/unix.c
+++ src/unix.c
@@ -211,17 +211,18 @@ static void set_process_group (void)
      {
 	pid_t pid = getpid ();
 	Startup_PGID = getpgid (pid);
-	if (-1 == tcsetpgrp (Read_FD, pid))
-	  {
-	     fprintf (stderr, "tcsetpgrp failed\n");
-	     Terminal_PGID = -1;
-	     return;
-	  }
 	if (-1 == setpgid (pid, pid))
 	  {
 	     fprintf (stderr, "setpgid failed\n");
 	     (void) tcsetpgrp (Read_FD, Startup_PGID);
 	     Terminal_PGID = -1;
+	     return;
+	  }
+	if (-1 == tcsetpgrp (Read_FD, pid))
+	  {
+	     fprintf (stderr, "tcsetpgrp failed: %s\n", SLerrno_strerror (errno));
+	     Terminal_PGID = -1;
+	     (void) setpgid (pid, Startup_PGID);
 	     return;
 	  }
      }
